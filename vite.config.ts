eb7c1cfceb95bf906b7d53822b3a45cbe2174e20
IGNITE-07 <jainym@rknec.edu>
2026-03-08 18:30:58 +0530

    fix render host + port

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
+    host: true,
+    port: Number(process.env.PORT) || 8080,
+    allowedHosts: [
+      "repo-analyzer-36es.onrender.com"
+    ]
   },
-  plugins: [react()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
